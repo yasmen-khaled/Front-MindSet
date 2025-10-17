3da52acad8f92b125e8952aee9720c4782a81ee1
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2025-10-17 21:28:15 +0200

    Add sum.py from Mindset quiz

---
+++ sum.py
@@ -0,0 +1,11 @@
+import random
+
+messages = [
+    "Keep going, you’re doing great!",
+    "Believe in yourself!",
+    "Small steps lead to big changes.",
+    "You got this!",
+    "Every day is a new chance!"
+]
+
+print(random.choice(messages))
